3e080585be2eb9cfaf02950e5131bcc4703bb068
Kathleen Geuens <k.geuens@hotmail.com>
2017-02-09 19:24:27 +0100

    uitvoer
    uitvoer toegevoegd!

--- VersieBeheer/Program.cs
+++ VersieBeheer/Program.cs
@@ -11,6 +11,8 @@ namespace VersieBeheer
         static void Main(string[] args)
         {
             Console.WriteLine("Gaat het een beetje?");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord + "? mooi zo, komt goed uit!");
             Console.ReadKey();
         }
     }
